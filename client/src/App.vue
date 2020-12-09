ff4224c0f5cd1cf6028d4f7da1e7d63544a4c396
Bosa7 <marko.bosnjak.bosa.93@gmail.com>
2020-12-09 19:52:21 +0100

    Update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,6 @@
 <template>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.0/css/all.css">
-	<router-view/>
+	<div>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.0/css/all.css">
+		<router-view/>
+	</div>
 </template>
